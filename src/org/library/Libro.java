344c0e870893b5dd274bdf03731695ead845a55e
DanieleSpuria <123416873+DanieleSpuria@users.noreply.github.com>
2023-09-19 14:39:39 +0200

    add Libro

---
+++ src/org/library/Libro.java
@@ -0,0 +1,50 @@
+package org.library;
+
+public class Libro {
+	private String titolo;
+	private int    pagine;
+	private String autore;
+	private String editore;
+	
+	public Libro (String titolo, int pagine, String autore, String editore) {
+		setTitolo(titolo);
+		setPagine(pagine);
+		setAutore(autore);
+		setEditore(editore);
+	}
+	
+	public String getTitolo() {
+		return titolo;
+	}
+	public int getPagine() {
+		return pagine;
+	}
+	public String getAutore() {
+		return autore;
+	}
+	public String getEditore() {
+		return editore;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	public void setPagine(int pagine) {
+		this.pagine = pagine;
+	}
+	public void setAutore(String autore) {
+		this.autore = autore;
+	}
+	public void setEditore(String editore) {
+
+		this.editore = editore;
+	}
+	
+	@Override
+	public String toString() {
+		return "Libro:\n" + 
+			   "Titolo: " + getTitolo() +
+			   "Autore: " + getAutore() +
+			   "Editore: "+ getEditore()+
+			   "Pagine :" + getPagine();
+	}
+}
